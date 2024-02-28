29990b782c960ab21d4682a125616554c8985eaf
Raliya <raliyashihan123@gmail.com>
2024-02-28 12:27:00 +0530

    Exercise 3.18

--- index.js
+++ index.js
@@ -44,16 +44,17 @@ app.get('/info', (request, response) => {
 	})
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+ })
 
 app.delete('/api/persons/:id', (request, response, next) => {
 	Person.findByIdAndDelete(request.params.id)
