c8e742ed53399febde6057ce539ae9b8b4e6f2f6
Raliya <raliyashihan123@gmail.com>
2024-03-05 12:08:25 +0530

    Exercises 3.19 - 3.21

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id'} )
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message})
 	}
 
 	next(error)
@@ -64,38 +66,29 @@ app.delete('/api/persons/:id', (request, response, next) => {
 		.catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name must be entered'
-    })
-  } else if (!body.number) {
-    return response.status(400).json({
-      error: 'number must be entered'
-    })
-  } else {
-		const person = new Person({
-			name: body.name,
-			number: body.number
-		})
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
 
-		person.save().then(savedPerson => {
+	person.save()
+		.then(savedPerson => {
 			response.json(savedPerson)
 		})
-	}
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-	const body = request.body
+	const { name, number } = request.body
 
-	const person = {
-		name: body.name,
-		number: body.number
-	}
-
-	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+	Person.findByIdAndUpdate(
+		request.params.id, 
+		{ name, number }, 
+		{ new: true, runValidators: true, context: 'query' }
+	)
 		.then(updatedPerson => {
 			response.json(updatedPerson)
 		})
--- models/person.js
+++ models/person.js
@@ -13,8 +13,22 @@ mongoose.connect(url)
 	})
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String
+	name: {
+		type: String,
+		minLength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		required: true,
+		validate: {
+			validator: 
+			function(v) {
+				return (/(0([0-9]{1,2})-([0-9]{7,}))/).test(v)
+			}
+		}
+	}
 })
 
 personSchema.set('toJSON', {
